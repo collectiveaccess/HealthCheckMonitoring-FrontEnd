efdbc63f15505fff8bca3ab3c2541361bcb910e2
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2024-05-16 07:21:16 -0700

    Revert "refactor ProjectsDetailPage to use api instead of database"
    This reverts commit f33fa31404bea54f7546cb2297f7b54f7bf7a408.

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,16 +1,15 @@
+import { fetch_project } from "@/lib/db_utils";
 import ProjectDetail from "@/components/projects/details/ProjectDetails";
+import { headers } from "next/headers";
 import { DEFAULT_PER_PAGE } from "@/data/constants";
 
-type Params = {
+export default async function ProjectsDetailPage({
+  params: { id },
+}: {
   params: { id: string };
-};
-
-export default async function ProjectsDetailPage({ params: { id } }: Params) {
-  const url = `${process.env.NEXT_PUBLIC_API_BASE}/projects/${id}`;
-  const res = await fetch(url, { next: { revalidate: 0 } });
-  const project = await res.json();
-
-  if (project.error) {
+}) {
+  const project = await fetch_project(id);
+  if (project === undefined) {
     return (
       <main className="container">
         <p>Invalid project id</p>
@@ -20,15 +19,19 @@ export default async function ProjectsDetailPage({ params: { id } }: Params) {
 
   const offset = 0;
   const perPage = Number(process.env.NEXT_PUBLIC_PER_PAGE) || DEFAULT_PER_PAGE;
-  const url2 = `${process.env.NEXT_PUBLIC_API_BASE}/project_status/${project.id}?offset=${offset}`;
-  const res2 = await fetch(url2);
-  const statuses = await res2.json();
+  const headersList = headers() as any;
+  const host = headersList.headers.host;
+  const proto = headersList.headers["x-forwarded-proto"];
+  const url = `${proto}://${host}/api/project_status/${project.id}?offset=${offset}`;
+
+  const res = await fetch(url);
+  const json = await res.json();
 
   return (
     <ProjectDetail
       project={project}
-      statuses={statuses.data}
-      statusesCount={statuses.count}
+      statuses={json.data}
+      statusesCount={json.count}
       perPage={perPage}
     />
   );
